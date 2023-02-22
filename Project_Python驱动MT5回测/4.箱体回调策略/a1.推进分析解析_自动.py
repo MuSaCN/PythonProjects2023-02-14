9977bc648033e44e4e06758cca9e136d5a462866
MuSa <39754824+MuSaCN@users.noreply.github.com>
2023-02-22 16:04:32 +0800

    updata

--- Project_Python驱动MT5回测/4.箱体回调策略/a1.推进分析解析_自动.py
+++ Project_Python驱动MT5回测/4.箱体回调策略/a1.推进分析解析_自动.py
@@ -94,7 +94,7 @@ FwdAuto.optcriterionaffix = myMT5run.get_optcriterion_affix(optcriterion=0) # **
 
 # (***)推进回测EA的目录(后面不能带\\)和文件名(***)
 FwdAuto.bt_experfolder = "My_Experts\\Strategy深度研究\\4.箱体回调策略\\推进交易.Balance_max.2Y6M"
-FwdAuto.bt_expertfile = "a1.f4.{}.{}.ex5".format("_Symbol", myMT5run.timeframe_to_ini_affix(FwdAuto.timeframe))
+FwdAuto.bt_expertfile = "a1.f4._Symbol.{}.ex5".format(myMT5run.timeframe_to_ini_affix(FwdAuto.timeframe))
 # (***)推进回测的时间起始(***)
 FwdAuto.bt_starttime = "2016.07.01"  # 手动指定******
 FwdAuto.bt_endtime = "2023.01.01"  # 手动指定******

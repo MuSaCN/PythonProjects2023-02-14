04b7f73f07ecc58fdc0f5d153a6d8da12812511d
MuSa <39754824+MuSaCN@users.noreply.github.com>
2023-03-13 12:30:31 +0800

    updata

--- Learning_MachineLearning/金融中的机器学习_Code/2.机器学习在结构化数据中的应用.py
+++ Learning_MachineLearning/金融中的机器学习_Code/2.机器学习在结构化数据中的应用.py
@@ -174,11 +174,578 @@ def plot_confusion_matrix(cm,
 
 #%%
 
-df = pd.read_csv('../input/PS_20174392719_1491204439457_log.csv')
+df = pd.read_csv(r'E:\学习\database数据集\金融中的机器学习\PS_20174392719_1491204439457_log.csv')
 df = df.rename(columns={'oldbalanceOrg':'oldBalanceOrig', 'newbalanceOrig':'newBalanceOrig', \
                         'oldbalanceDest':'oldBalanceDest', 'newbalanceDest':'newBalanceDest'})
+len(df)
+
+#%%
+df.head()
+
+#%%
+# 查看诈骗交易的交易类型 'TRANSFER', 'CASH_OUT'
+df.loc[df.isFraud == 1].type.drop_duplicates().values
+df.loc[df.isFraud == 1].type.unique()
+
+#%%
+# 只研究这两者类型的交易
+df = df[(df.type == 'TRANSFER') | (df.type == 'CASH_OUT')]
 
 #%%
 
+len(df)
+
+#%%
+# 诈骗交易且类型为 TRANSFER 的交易类中位数
+df.loc[(df.isFraud == 1) & (df.type == 'TRANSFER')].amount.median()
+
+#%%
+# 非诈骗交易且类型为 TRANSFER 的交易类中位数
+df.loc[(df.isFraud == 0) & (df.type == 'TRANSFER')].amount.median()
+
+#%%
+# 使用启发式模式来预测，创建 Fraud_Heuristic 列.
+# 满足条件1，否则0.
+df['Fraud_Heuristic'] = np.where(((df['type'] == 'TRANSFER') & (df['amount'] > 200000)),1,0)
+
+#%%
+
+df['Fraud_Heuristic'].sum()
+
+# %%
+# 调和平均数处理不可相加，但是混合的事物。关键点在于混合。
+# f1分数为准确率和召回率的调和平均数.
+from sklearn.metrics import f1_score
+
+# %%
+
+f1_score(y_pred=df['Fraud_Heuristic'], y_true=df['isFraud'])
+
+# %%
+
+from sklearn.metrics import confusion_matrix
+
+# %%
+
+cm = confusion_matrix(y_pred=df['Fraud_Heuristic'], y_true=df['isFraud'])
+
+# %%
+
+plot_confusion_matrix(cm, ['Genuine', 'Fraud'], normalize=False)
+
+# %%
+
+df.shape
+
+# %%
+
+df['hour'] = df['step'] % 24
+
+# %%
+
+frauds = []
+genuine = []
+for i in range(24):
+    f = len(df[(df['hour'] == i) & (df['isFraud'] == 1)])
+    g = len(df[(df['hour'] == i) & (df['isFraud'] == 0)])
+    frauds.append(f)
+    genuine.append(g)
+
+# %%
+
+
+# %%
+
+sns.set_style("white")
+
+fig, ax = plt.subplots(figsize=(10, 6))
+gen = ax.plot(genuine / np.sum(genuine), label='Genuine')
+fr = ax.plot(frauds / np.sum(frauds), dashes=[5, 2], label='Fraud')
+# frgen = ax.plot(np.devide(frauds,genuine),dashes=[1, 1], label='Fraud vs Genuine')
+plt.xticks(np.arange(24))
+legend = ax.legend(loc='upper center', shadow=True)
+fig.savefig('time.png')
+
+# %%
+
+sns.set_style("white")
+
+fig, ax = plt.subplots(figsize=(10, 6))
+# gen = ax.plot(genuine/np.sum(genuine), label='Genuine')
+# fr = ax.plot(frauds/np.sum(frauds),dashes=[5, 2], label='Fraud')
+frgen = ax.plot(np.divide(frauds, np.add(genuine, frauds)), label='Share of fraud')
+plt.xticks(np.arange(24))
+legend = ax.legend(loc='upper center', shadow=True)
+fig.savefig('time_comp.png')
+
+# %%
+
+dfFraudTransfer = df[(df.isFraud == 1) & (df.type == 'TRANSFER')]
+
+# %%
+
+dfFraudCashOut = df[(df.isFraud == 1) & (df.type == 'CASH_OUT')]
+
+# %%
+
+dfFraudTransfer.nameDest.isin(dfFraudCashOut.nameOrig).any()
+
+# %%
+
+dfNotFraud = df[(df.isFraud == 0)]
+
+# %%
+
+dfFraud = df[(df.isFraud == 1)]
+
+# %%
+
+dfFraudTransfer.loc[dfFraudTransfer.nameDest.isin(
+    dfNotFraud.loc[dfNotFraud.type == 'CASH_OUT'].nameOrig.drop_duplicates())]
+
+# %%
+
+len(dfFraud[(dfFraud.oldBalanceDest == 0) & (dfFraud.newBalanceDest == 0) & (dfFraud.amount)]) / (1.0 * len(dfFraud))
+
+# %%
+
+len(dfNotFraud[(dfNotFraud.oldBalanceDest == 0) & (dfNotFraud.newBalanceDest == 0) & (dfNotFraud.amount)]) / (
+            1.0 * len(dfNotFraud))
+
+# %%
+
+dfOdd = df[(df.oldBalanceDest == 0) &
+           (df.newBalanceDest == 0) &
+           (df.amount)]
+
+# %%
+
+len(dfOdd[(dfOdd.isFraud == 1)]) / len(dfOdd)
+
+# %%
+
+len(dfOdd[(dfOdd.oldBalanceOrig <= dfOdd.amount)]) / len(dfOdd)
+
+# %%
+
+len(dfOdd[(dfOdd.oldBalanceOrig <= dfOdd.amount) & (dfOdd.isFraud == 1)]) / len(dfOdd[(dfOdd.isFraud == 1)])
+
+# %%
+
+dfOdd.columns
+
+# %%
+
+dfOdd.head(20)
+
+# %%
+
 df.head()
+
+# %%
+
+df['type'] = 'type_' + df['type'].astype(str)
+
+# %%
+
+# Get dummies
+dummies = pd.get_dummies(df['type'])
+
+# Add dummies to df
+df = pd.concat([df, dummies], axis=1)
+
+# remove original column
+del df['type']
+
+# %% md
+
+Predictive
+modeling
+with Keras
+
+# %%
+
+df = df.drop(['nameOrig', 'nameDest', 'Fraud_Heuristic'], axis=1)
+
+# %%
+
+df['isNight'] = np.where((2 <= df['hour']) & (df['hour'] <= 6), 1, 0)
+
+# %%
+
+df[df['isNight'] == 1].isFraud.mean()
+
+# %%
+
+df.head()
+
+# %%
+
+df = df.drop(['step', 'hour'], axis=1)
+
+# %%
+
+df.head()
+
+# %%
+
+df.columns.values
+
+# %%
+
+y_df = df['isFraud']
+x_df = df.drop('isFraud', axis=1)
+
+# %%
+
+y = y_df.values
+X = x_df.values
+
+# %%
+
+y.shape
+
+# %%
+
+X.shape
+
+# %%
+
+from sklearn.model_selection import train_test_split
+
+# %%
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.33,
+                                                    random_state=42)
+
+# %%
+
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
+                                                  test_size=0.1,
+                                                  random_state=42)
+
+# %%
+
+from imblearn.over_sampling import SMOTE, RandomOverSampler
+
+# %%
+
+sm = SMOTE(random_state=42)
+X_train_res, y_train_res = sm.fit_sample(X_train, y_train)
+
+# %%
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.optimizers import SGD
+
+# %%
+
+# Log reg
+model = Sequential()
+model.add(Dense(1, input_dim=9))
+model.add(Activation('sigmoid'))
+
+# %%
+
+model.summary()
+
+# %%
+
+model.compile(loss='binary_crossentropy',
+              optimizer=SGD(lr=1e-5),
+              metrics=['acc'])
+
+# %%
+
+model.fit(X_train_res, y_train_res,
+          epochs=5,
+          batch_size=256,
+          validation_data=(X_val, y_val))
+
+# %%
+
+y_pred = model.predict(X_test)
+
+# %%
+
+y_pred[y_pred > 0.5] = 1
+y_pred[y_pred < 0.5] = 0
+
+# %%
+
+f1_score(y_pred=y_pred, y_true=y_test)
+
+# %%
+
+cm = confusion_matrix(y_pred=y_pred, y_true=y_test)
+
+# %%
+
+plot_confusion_matrix(cm, ['Genuine', 'Fraud'], normalize=False)
+
+# %%
+
+model = Sequential()
+model.add(Dense(16, input_dim=9))
+model.add(Activation('tanh'))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+# %%
+
+model.compile(loss='binary_crossentropy', optimizer=SGD(lr=1e-4), metrics=['acc'])
+
+# %%
+
+model.fit(X_train_res, y_train_res,
+          epochs=5, batch_size=256,
+          validation_data=(X_val, y_val))
+
+# %%
+
+y_pred = model.predict(X_test)
+
+# %%
+
+y_pred[y_pred > 0.5] = 1
+y_pred[y_pred < 0.5] = 0
+
+# %%
+
+f1_score(y_pred=y_pred, y_true=y_test)
+
+# %%
+
+cm = confusion_matrix(y_pred=y_pred, y_true=y_test)
+
+# %%
+
+plot_confusion_matrix(cm, ['Genuine', 'Fraud'], normalize=False)
+
+# %% md
+
+# Tree based methods
+
+# %%
+
+from sklearn.tree import export_graphviz
+
+# %%
+
+from sklearn.tree import DecisionTreeClassifier
+
+dtree = DecisionTreeClassifier()
+dtree.fit(X_train, y_train)
+
+# %%
+
+from sklearn.externals.six import StringIO
+from IPython.display import Image
+from sklearn.tree import export_graphviz
+from subprocess import check_call
+from PIL import Image, ImageDraw, ImageFont
+from IPython.display import Image as PImage
+
+# import pydotplus
+dot_data = StringIO()
+'''export_graphviz(dtree, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True)'''
+with open("tree1.dot", 'w') as f:
+    f = export_graphviz(dtree,
+                        out_file=f,
+                        max_depth=3,
+                        impurity=True,
+                        feature_names=list(df.drop(['isFraud'], axis=1)),
+                        class_names=['Genuine', 'Fraud'],
+                        rounded=True,
+                        filled=True)
+
+# Convert .dot to .png to allow display in web notebook
+check_call(['dot', '-Tpng', 'tree1.dot', '-o', 'tree1.png'])
+
+# Annotating chart with PIL
+img = Image.open("tree1.png")
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype('/usr/share/fonts/truetype/liberation/LiberationSerif-Bold.ttf', 26)
+img.save('sample-out.png')
+PImage("sample-out.png")
+
+# %%
+
+from sklearn.ensemble import RandomForestClassifier
+
+# %%
+
+rf = RandomForestClassifier(n_estimators=10, n_jobs=-1)
+rf.fit(X_train, y_train)
+
+# %%
+
+y_pred = rf.predict(X_test)
+
+# %%
+
+f1_score(y_pred=y_pred, y_true=y_test)
+
+# %%
+
+cm = confusion_matrix(y_pred=y_pred, y_true=y_test)
+plot_confusion_matrix(cm, ['Genuine', 'Fraud'], normalize=False)
+
+# %%
+
+import xgboost as xgb
+
+# %%
+
+booster = xgb.XGBClassifier(n_jobs=-1)
+booster = booster.fit(X_train, y_train)
+
+# %%
+
+y_pred = booster.predict(X_test)
+
+# %%
+
+f1_score(y_pred=y_pred, y_true=y_test)
+
+# %%
+
+cm = confusion_matrix(y_pred=y_pred, y_true=y_test)
+plot_confusion_matrix(cm, ['Genuine', 'Fraud'], normalize=False)
+
+# %% md
+
+# Entity embeddings
+
+# %%
+
+# Reload data
+df = pd.read_csv('../input/PS_20174392719_1491204439457_log.csv')
+df = df.rename(columns={'oldbalanceOrg': 'oldBalanceOrig', 'newbalanceOrig': 'newBalanceOrig', \
+                        'oldbalanceDest': 'oldBalanceDest', 'newbalanceDest': 'newBalanceDest'})
+
+# %%
+
+df.head()
+
+# %%
+
+df = df.drop(['nameDest', 'nameOrig', 'step'], axis=1)
+
+# %%
+
+df['type'].unique()
+
+# %%
+
+map_dict = {}
+for token, value in enumerate(df['type'].unique()):
+    map_dict[value] = token
+
+# %%
+
+map_dict
+
+# %%
+
+df["type"].replace(map_dict, inplace=True)
+
+# %%
+
+df.head()
+
+# %%
+
+other_cols = [c for c in df.columns if ((c != 'type') and (c != 'isFraud'))]
+
+# %%
+
+other_cols
+
+# %%
+
+from keras.models import Model
+from keras.layers import Embedding, Merge, Dense, Activation, Reshape, Input, Concatenate
+
+# %%
+
+num_types = len(df['type'].unique())
+type_embedding_dim = 3
+
+# %%
+
+inputs = []
+outputs = []
+
+# %%
+
+type_in = Input(shape=(1,))
+type_embedding = Embedding(num_types, type_embedding_dim, input_length=1)(type_in)
+type_out = Reshape(target_shape=(type_embedding_dim,))(type_embedding)
+
+type_model = Model(type_in, type_out)
+
+inputs.append(type_in)
+outputs.append(type_out)
+
+# %%
+
+num_rest = len(other_cols)
+
+# %%
+
+rest_in = Input(shape=(num_rest,))
+rest_out = Dense(16)(rest_in)
+
+rest_model = Model(rest_in, rest_out)
+
+inputs.append(rest_in)
+outputs.append(rest_out)
+
+# %%
+
+concatenated = Concatenate()(outputs)
+
+# %%
+
+x = Dense(16)(concatenated)
+x = Activation('sigmoid')(x)
+x = Dense(1)(concatenated)
+model_out = Activation('sigmoid')(x)
+
+# %%
+
+merged_model = Model(inputs, model_out)
+merged_model.compile(loss='binary_crossentropy',
+                     optimizer='adam',
+                     metrics=['accuracy'])
+
+# %%
+
+types = df['type']
+
+# %%
+
+rest = df[other_cols]
+
+# %%
+
+target = df['isFraud']
+
+# %%
+
+history = merged_model.fit([types.values, rest.values], target.values,
+                           epochs=1, batch_size=128)
+
+# %%
+
+merged_model.summary()
+
+# %%
+
+
 

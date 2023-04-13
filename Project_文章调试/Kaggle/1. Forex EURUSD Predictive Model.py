d751af06125302d904cac997c8cddc339a4d25c5
MuSa <39754824+MuSaCN@users.noreply.github.com>
2023-04-13 20:05:04 +0800

    updata

---
+++ Project_文章调试/Kaggle/1. Forex EURUSD Predictive Model.py
@@ -0,0 +1,300 @@
+# Author:Zhang Yuan
+from MyPackage import *
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import seaborn as sns
+import statsmodels.api as sm
+from scipy import stats
+
+# ------------------------------------------------------------
+__mypath__ = MyPath.MyClass_Path("")  # 路径类
+mylogging = MyDefault.MyClass_Default_Logging(activate=False)  # 日志记录类，需要放在上面才行
+myfile = MyFile.MyClass_File()  # 文件操作类
+myword = MyFile.MyClass_Word()  # word生成类
+myexcel = MyFile.MyClass_Excel()  # excel生成类
+myini = MyFile.MyClass_INI()  # ini文件操作类
+mytime = MyTime.MyClass_Time()  # 时间类
+myparallel = MyTools.MyClass_ParallelCal()  # 并行运算类
+myplt = MyPlot.MyClass_Plot()  # 直接绘图类(单个图窗)
+mypltpro = MyPlot.MyClass_PlotPro()  # Plot高级图系列
+myfig = MyPlot.MyClass_Figure(AddFigure=False)  # 对象式绘图类(可多个图窗)
+myfigpro = MyPlot.MyClass_FigurePro(AddFigure=False)  # Figure高级图系列
+myplthtml = MyPlot.MyClass_PlotHTML()  # 画可以交互的html格式的图
+mynp = MyArray.MyClass_NumPy()  # 多维数组类(整合Numpy)
+mypd = MyArray.MyClass_Pandas()  # 矩阵数组类(整合Pandas)
+mypdpro = MyArray.MyClass_PandasPro()  # 高级矩阵数组类
+myDA = MyDataAnalysis.MyClass_DataAnalysis()  # 数据分析类
+myDefault = MyDefault.MyClass_Default_Matplotlib()  # 画图恢复默认设置类
+# myMql = MyMql.MyClass_MqlBackups() # Mql备份类
+# myBaidu = MyWebCrawler.MyClass_BaiduPan() # Baidu网盘交互类
+# myImage = MyImage.MyClass_ImageProcess()  # 图片处理类
+myBT = MyBackTest.MyClass_BackTestEvent()  # 事件驱动型回测类
+myBTV = MyBackTest.MyClass_BackTestVector()  # 向量型回测类
+myML = MyMachineLearning.MyClass_MachineLearning()  # 机器学习综合类
+mySQL = MyDataBase.MyClass_MySQL(connect=False)  # MySQL类
+mySQLAPP = MyDataBase.MyClass_SQL_APPIntegration()  # 数据库应用整合
+myWebQD = MyWebCrawler.MyClass_QuotesDownload(tushare=False)  # 金融行情下载类
+myWebR = MyWebCrawler.MyClass_Requests()  # Requests爬虫类
+myWebS = MyWebCrawler.MyClass_Selenium(openChrome=False)  # Selenium模拟浏览器类
+myWebAPP = MyWebCrawler.MyClass_Web_APPIntegration()  # 爬虫整合应用类
+myEmail = MyWebCrawler.MyClass_Email()  # 邮箱交互类
+myReportA = MyQuant.MyClass_ReportAnalysis()  # 研报分析类
+myFactorD = MyQuant.MyClass_Factor_Detection()  # 因子检测类
+myKeras = MyDeepLearning.MyClass_tfKeras()  # tfKeras综合类
+myTensor = MyDeepLearning.MyClass_TensorFlow()  # Tensorflow综合类
+myMT5 = MyMql.MyClass_ConnectMT5(connect=False)  # Python链接MetaTrader5客户端类
+myMT5Pro = MyMql.MyClass_ConnectMT5Pro(connect=False)  # Python链接MT5高级类
+myMT5Indi = MyMql.MyClass_MT5Indicator()  # MT5指标Python版
+myMT5Report = MyMT5Report.MyClass_StratTestReport(AddFigure=False)  # MT5策略报告类
+myMT5Analy = MyMT5Analysis.MyClass_ForwardAnalysis()  # MT5分析类
+myMT5Lots_Fix = MyMql.MyClass_Lots_FixedLever(connect=False)  # 固定杠杆仓位类
+myMT5Lots_Dy = MyMql.MyClass_Lots_DyLever(connect=False)  # 浮动杠杆仓位类
+myMT5run = MyMql.MyClass_RunningMT5()  # Python运行MT5
+myMT5code = MyMql.MyClass_CodeMql5()  # Python生成MT5代码
+myMoneyM = MyTrade.MyClass_MoneyManage()  # 资金管理类
+myDefault.set_backend_default("Pycharm")  # Pycharm下需要plt.show()才显示图
+# ------------------------------------------------------------
+# Jupyter Notebook 控制台显示必须加上：%matplotlib inline ，弹出窗显示必须加上：%matplotlib auto
+# %matplotlib inline
+# import warnings
+# warnings.filterwarnings('ignore')
+
+# %%
+#ignoring warnings
+import warnings
+warnings.simplefilter('ignore')
+
+#importing neccesary modules
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import seaborn as sns
+
+import sklearn
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import BaggingRegressor, RandomForestRegressor
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import joblib # from sklearn.externals import joblib
+from sklearn.model_selection import GridSearchCV, train_test_split
+
+import xgboost
+from xgboost import XGBRegressor, DMatrix
+
+
+#%%
+eu = myMT5Pro.getsymboldata("EURUSD", "TIMEFRAME_D1", [1970,1,1,0,0,0], [2024,1,1,0,0,0], index_time=True,col_capitalize=False)
+eu = eu[["open","high","low","close","tick_volume"]]
+eu.columns = ["open","high","low","close","volume"]
+
+eu.head(2)
+
+eu.isna().sum()
+
+plt.figure(figsize=(15,6))
+plt.plot(eu.close)
+plt.title('Euro vs USD')
+plt.legend()
+plt.show()
+
+# box plot for open, high, low , close
+eu.boxplot()
+plt.show()
+eu.drop('volume', axis=1).boxplot()
+plt.show()
+
+# 移除交易量超过均值3倍标准差
+vol_cut_off = eu.volume.std()*3 + eu.volume.mean()
+eu.volume[eu.volume > vol_cut_off] = vol_cut_off
+
+# removing ouliers below
+vol_cut_off =  eu.volume.mean() - eu.volume.std()*3
+eu.volume[eu.volume < vol_cut_off] = vol_cut_off
+
+# boxplot for volume
+eu[['volume']].boxplot()
+plt.show()
+
+#%%
+
+def generate_features(df):
+    """ Generate features for a stock/index/currency/commodity based on historical price and performance
+    Args:
+        df (dataframe with columns "open", "close", "high", "low", "volume")
+    Returns:
+        dataframe, data set with new features
+    """
+    df_new = pd.DataFrame()
+
+    # 6 original features
+    df_new['open'] = df['open']
+    df_new['open_1'] = df['open'].shift(1)
+    df_new['close_1'] = df['close'].shift(1)
+    df_new['high_1'] = df['high'].shift(1)
+    df_new['low_1'] = df['low'].shift(1)
+    df_new['volume_1'] = df['volume'].shift(1)
+
+    # 50 original features
+    # average price
+    df_new['avg_price_5'] = df['close'].rolling(window=5).mean().shift(1)
+    df_new['avg_price_30'] = df['close'].rolling(window=21).mean().shift(1)
+    df_new['avg_price_90'] = df['close'].rolling(window=63).mean().shift(1)
+    df_new['avg_price_365'] = df['close'].rolling(window=252).mean().shift(1)
+
+    # average price ratio
+    df_new['ratio_avg_price_5_30'] = df_new['avg_price_5'] / df_new['avg_price_30']
+    df_new['ratio_avg_price_5_90'] = df_new['avg_price_5'] / df_new['avg_price_90']
+    df_new['ratio_avg_price_5_365'] = df_new['avg_price_5'] / df_new['avg_price_365']
+    df_new['ratio_avg_price_30_90'] = df_new['avg_price_30'] / df_new['avg_price_90']
+    df_new['ratio_avg_price_30_365'] = df_new['avg_price_30'] / df_new['avg_price_365']
+    df_new['ratio_avg_price_90_365'] = df_new['avg_price_90'] / df_new['avg_price_365']
+
+    # average volume
+    df_new['avg_volume_5'] = df['volume'].rolling(window=5).mean().shift(1)
+    df_new['avg_volume_30'] = df['volume'].rolling(window=21).mean().shift(1)
+    df_new['avg_volume_90'] = df['volume'].rolling(window=63).mean().shift(1)
+    df_new['avg_volume_365'] = df['volume'].rolling(window=252).mean().shift(1)
+
+    # average volume ratio
+    df_new['ratio_avg_volume_5_30'] = df_new['avg_volume_5'] / df_new['avg_volume_30']
+    df_new['ratio_avg_volumee_5_90'] = df_new['avg_volume_5'] / df_new['avg_volume_90']
+    df_new['ratio_avg_volume_5_365'] = df_new['avg_volume_5'] / df_new['avg_volume_365']
+    df_new['ratio_avg_volume_30_90'] = df_new['avg_volume_30'] / df_new['avg_volume_90']
+    df_new['ratio_avg_volume_30_365'] = df_new['avg_volume_30'] / df_new['avg_volume_365']
+    df_new['ratio_avg_volume_90_365'] = df_new['avg_volume_90'] / df_new['avg_volume_365']
+
+    # 移动价格的std
+    df_new['std_price_5'] = df['close'].rolling(window=5).std().shift(1)
+    df_new['std_price_30'] = df['close'].rolling(window=21).std().shift(1)
+    df_new['std_price_90'] = df['close'].rolling(window=63).std().shift(1)
+    df_new['std_price_365'] = df['close'].rolling(window=252).std().shift(1)
+
+    # standard deviation ratio of prices
+    df_new['ratio_std_price_5_30'] = df_new['std_price_5'] / df_new['std_price_30']
+    df_new['ratio_std_price_5_90'] = df_new['std_price_5'] / df_new['std_price_90']
+    df_new['ratio_std_price_5_365'] = df_new['std_price_5'] / df_new['std_price_365']
+    df_new['ratio_std_price_30_90'] = df_new['std_price_30'] / df_new['std_price_90']
+    df_new['ratio_std_price_30_365'] = df_new['std_price_30'] / df_new['std_price_365']
+    df_new['ratio_std_price_90_365'] = df_new['std_price_90'] / df_new['std_price_365']
+
+    # standard deviation of volumes
+    df_new['std_volume_5'] = df['volume'].rolling(window=5).std().shift(1)
+    df_new['std_volume_30'] = df['volume'].rolling(window=21).std().shift(1)
+    df_new['std_volume_90'] = df['volume'].rolling(window=63).std().shift(1)
+    df_new['std_volume_365'] = df['volume'].rolling(window=252).std().shift(1)
+
+    # standard deviation ratio of volumes
+    df_new['ratio_std_volume_5_30'] = df_new['std_volume_5'] / df_new['std_volume_30']
+    df_new['ratio_std_volume_5_90'] = df_new['std_volume_5'] / df_new['std_volume_90']
+    df_new['ratio_std_volume_5_365'] = df_new['std_volume_5'] / df_new['std_volume_365']
+    df_new['ratio_std_volume_30_90'] = df_new['std_volume_30'] / df_new['std_volume_90']
+    df_new['ratio_std_volume_30_365'] = df_new['std_volume_30'] / df_new['std_volume_365']
+    df_new['ratio_std_volume_90_365'] = df_new['std_volume_90'] / df_new['std_volume_365']
+
+    # return
+    df_new['return_1'] = ((df['close'] - df['close'].shift(1)) / df['close'].shift(1)).shift(1)
+    df_new['return_5'] = ((df['close'] - df['close'].shift(5)) / df['close'].shift(5)).shift(1)
+    df_new['return_30'] = ((df['close'] - df['close'].shift(21)) / df['close'].shift(21)).shift(1)
+    df_new['return_90'] = ((df['close'] - df['close'].shift(63)) / df['close'].shift(63)).shift(1)
+    df_new['return_365'] = ((df['close'] - df['close'].shift(252)) / df['close'].shift(252)).shift(1)
+
+    # average of return
+    df_new['moving_avg_5'] = df_new['return_1'].rolling(window=5).mean()
+    df_new['moving_avg_30'] = df_new['return_1'].rolling(window=21).mean()
+    df_new['moving_avg_30'] = df_new['return_1'].rolling(window=63).mean()
+    df_new['moving_avg_365'] = df_new['return_1'].rolling(window=252).mean()
+
+    # the target
+    df_new['close'] = df['close']
+    df_new = df_new.dropna(axis=0)
+    return df_new
+
+
+data = generate_features(df = eu)
+data.head()
+data.describe()
+data.info()
+
+#%%
+sns.heatmap(data.corr())
+plt.show()
+
+#%%
+#import datetime module
+import datetime
+
+#segregate data for training
+start_train = datetime.datetime(1999, 1, 1,0,0)
+end_train = datetime.datetime(2017, 12, 31, 0, 0)
+data_train = data.loc[start_train:end_train]
+data_train.describe()
+
+#segregate data for validation
+start_test = datetime.datetime(2018, 1, 1, 0, 0)
+end_test = datetime.datetime(2019, 6, 7, 0, 0)
+data_test = data.loc[start_test:end_test]
+data_test.describe()
+
+#%%
+X_train = data_train.drop('close', axis='columns')
+y_train = data_train.close
+
+X_test = data_test.drop('close', axis='columns')
+y_test = data_test.close
+
+#checking the shape of the train and test data
+X_train.shape, X_test.shape, y_train.shape, y_test.shape
+
+
+#%% SCALING THE PREDICTOR DATA
+#initiating standard scaler
+# 将数据按其属性(按列进行)减去其均值，然后除以其方差。最后得到的结果是，对每个属性/每列来说所有数据都聚集在0附近，方差值为1。
+scaler = StandardScaler()
+
+# 除去nan，正负无限的结果
+choose = ~X_train.isin([np.nan, np.inf, -np.inf]).any(1)
+X_train = X_train[choose]
+y_train = y_train[choose]
+
+#fit the scaler in training features
+scaler.fit(X_train)
+
+# Rescale both sets using the trained scaler
+X_scaled_train = scaler.transform(X_train)
+X_scaled_test = scaler.transform(X_test)
+
+
+#%% LINEAR REGRESSION
+from sklearn.linear_model import LinearRegression
+lin = LinearRegression()
+
+lin.fit(X_scaled_train, y_train)
+predictions_lin = lin.predict(X_scaled_test)
+
+print('RMSE: {0:.3f}'.format(mean_squared_error(y_test, predictions_lin)**0.5))
+print('MAE: {0:.3f}'.format(mean_absolute_error(y_test, predictions_lin)))
+print('R^2: {0:.3f}'.format(r2_score(y_test, predictions_lin)))
+
+#%% 以测试集来展示下预测结果与实际结果
+dates = data_test.index.values
+plt.figure(figsize = (18,9))
+plt.style.use('seaborn-whitegrid')
+plot_truth, = plt.plot(dates, y_test)
+plot_lin, = plt.plot(dates, predictions_lin)
+plt.legend([plot_truth, plot_lin], ['Truth', 'Linear Regression'])
+plt.title('Gold price : Prediction vs Truth - Linear Regression')
+plt.show()
+
+
+#%% SGD REGRESSOR (SGD)
+
+
+
+
+
